5f802a0ce0284ed0e5ef031368575fc6347f3ad8
Laura-1557 <94494994+Laura-1557@users.noreply.github.com>
2021-12-31 21:14:57 +0000

    Add files via upload
    Ran past a couple of friends, one commented on the use of int(str) could be simplified by changing the initial variable int(input(str)).
    Then running in Pycharm looked at the error messages. I cleaned up the code to make it more readable, robust and simplified.

---
+++ Laura's Self Project - cleaned.py
@@ -0,0 +1,50 @@
+def main():
+
+    import datetime
+    x = datetime.datetime.now()
+
+    name = input("What is your name? ")
+
+    if name == "Laura":
+        print("Hello Laura!")
+        laura_age = int(input("How old are you? "))
+        if laura_age < 40:
+            print("You're a Spring Chicken!")
+        if laura_age >= 40:
+            print("Life begins at 40 - You're in your prime!")
+        main()
+
+    elif name == "Adam":
+        print("Hi Adam!")
+        day_of_week = input("What day is it today? ")
+        is_weekend = "Saturday" or "Sunday"
+        if day_of_week == x.strftime("%A") == is_weekend:
+            print("Nothing gets past you! Have a great weekend")
+        elif day_of_week == x.strftime("%A") != is_weekend:
+            print("Nothing gets past you! Have a great day at work")
+        elif day_of_week != x.strftime("%A"):
+            print("You might want to buy a calendar")
+        main()
+
+    elif name == "Serge":
+        print("Quack! Try another name!")
+        main()
+
+    elif name == "Nick" or "nick":
+        number_of_computer_monitors = int(input("How many computer monitors do you have? "))
+        if number_of_computer_monitors <= 2:
+            print("That's not too bad.")
+        elif 3 <= number_of_computer_monitors < 5:
+            print("Are you sure you need that many?")
+        elif number_of_computer_monitors >= 5:
+            print("Are you NASA???")
+        print("Try a different name - ")
+        main()
+
+    else:
+        print("Sorry, wrong person")
+        main()
+
+
+if __name__ == '__main__':
+    main()/n
